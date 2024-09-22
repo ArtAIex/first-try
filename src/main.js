978f6382b385382dd8fb62205ab9206d459b5c86
Артём <artalex@MacBook-Pro-Artem.local>
2024-09-22 19:32:00 +0300

    game mechanics beta

--- src/main.js
+++ src/main.js
@@ -1,14 +1,5 @@
 import { Chess } from '/node_modules/chess.js'
 
-function createPiece(type, color) {
-  //       #:(
-  const piece = document.createElement('img');
-  piece.classList.add('piece');
-  piece.draggable = 'true';
-  piece.src = 'images/' + color + type + '.png';
-  return piece;
-}
-
 function setBoardSize() {
   const div = document.getElementById('board');
   const parent = document.getElementById('content');
@@ -30,6 +21,16 @@ function setBoardSize() {
   }
 }
 
+function createPiece(type, color) {
+  //       #:(
+  const piece = document.createElement('img');
+  piece.classList.add('piece');
+  piece.draggable = 'true';
+  piece.src = 'images/' + color + type + '.png';
+  piece.dataset.color = color;
+  return piece;
+}
+
 const boardSize = 8;
 const board = document.createElement('div');
 board.classList.add('chess_board');
@@ -45,7 +46,6 @@ for (let i = 0; i < boardSize; i++) {
     cell.dataset.row = i;
     cell.dataset.col = j;
     cell.classList.add('.droppable');
-
     board.appendChild(cell);
   }
 }
@@ -54,6 +54,20 @@ const content = document.getElementById('content');
 content.appendChild(board);
 
 const chess = new Chess();
+
+function isValidMove(startCell, currentCell) {
+  let startCellString = String.fromCharCode(97 + Number(startCell.dataset.col)) + (Number(startCell.dataset.row) + 1);
+  let currentCellString = String.fromCharCode(97 + Number(currentCell.dataset.col)) + (Number(currentCell.dataset.row) + 1);
+  let move = chess.get(startCellString).type + startCellString + '-' + currentCellString;
+  try {
+    chess.move({ from: startCellString, to: currentCellString });
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+  return true;
+}
+
 for (let i = 0; i < boardSize; i++) {
   for (let j = 0; j < boardSize; j++) {
     let column = String.fromCharCode(97 + j);
@@ -65,23 +79,49 @@ for (let i = 0; i < boardSize; i++) {
       };
 
       currentPiece.onmousedown = function(event) {
+        if (chess.turn() != currentPiece.dataset.color) {
+          return;
+        }
         currentPiece.style.position = 'absolute';
         currentPiece.style.zIndex = 1000;
         board.append(currentPiece);
         setBoardSize();
-        function moveAt(pageX, pageY) {
+        function moveAt(old, pageX, pageY) {
           currentPiece.hidden = true;
-          let elemBelow = document.elementFromPoint(pageX, pageY);
+          let elemBelow = document.elementFromPoint(old.X, pageY);
+          currentPiece.hidden = false;
+          if (elemBelow.classList.contains('chess_cell') || elemBelow.classList.contains('piece')) {
+            currentPiece.style.top = pageY - currentPiece.offsetHeight / 2 + 'px';
+            old.Y = pageY;
+          }
+          currentPiece.hidden = true;
+          elemBelow = document.elementFromPoint(pageX, old.Y);
           currentPiece.hidden = false;
           if (elemBelow.classList.contains('chess_cell') || elemBelow.classList.contains('piece')) {
             currentPiece.style.left = pageX - currentPiece.offsetWidth / 2 + 'px';
-            currentPiece.style.top = pageY - currentPiece.offsetHeight / 2 + 'px';
+            old.X = pageX;
           }
         }
-        moveAt(event.pageX, event.pageY);
+        let old = {
+          X: event.pageX,
+          Y: event.pageY
+        };
+        moveAt(old, event.pageX, event.pageY);
+
+        function getCell(X, Y) {
+          let elemBelow = document.elementFromPoint(X, Y);
+          let result = elemBelow;
+          if (elemBelow.classList.contains('piece')) {
+            elemBelow.hidden = true;
+            result = document.elementFromPoint(X, Y);
+            elemBelow.hidden = false;
+          }
+          return result;
+        }
+
         currentPiece.hidden = true;
-        let currentCell = document.elementFromPoint(event.clientX, event.clientY);
-        let start = currentCell;
+        let currentCell = getCell(event.clientX, event.clientY);
+        let startCell = getCell(event.clientX, event.clientY);
         currentPiece.hidden = false;
 
         function enterDiv(container) {
@@ -97,11 +137,9 @@ for (let i = 0; i < boardSize; i++) {
 
         enterDiv(currentCell);
         function onMouseMove(event) {
-          moveAt(event.pageX, event.pageY);
-          console.log(event.clientX, event.clientY);
-          console.log(event.pageX, event.pageY);
+          moveAt(old, event.pageX, event.pageY);
           currentPiece.hidden = true;
-          let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+          let elemBelow = getCell(old.X, old.Y);
           currentPiece.hidden = false;
           if (!elemBelow) return;
           if (elemBelow != currentCell) {
@@ -115,17 +153,25 @@ for (let i = 0; i < boardSize; i++) {
           }
         }
         document.addEventListener('mousemove', onMouseMove);
-        currentPiece.onmouseup = function() {
+        function mouseUp(event) {
           document.removeEventListener('mousemove', onMouseMove);
-          currentPiece.onmouseup = null;
-          console.log(currentCell);
-          if (currentCell) {
+          document.removeEventListener('mouseup', mouseUp);
+          currentPiece.hidden = true;
+          let elemBelow = document.elementFromPoint(event.pageX, event.pageY);
+          currentPiece.hidden = false;
+          if (isValidMove(startCell, currentCell) && currentCell.classList.contains('chess_cell')) {
+            if (elemBelow.classList.contains('piece')) {
+              currentCell.removeChild(elemBelow);
+            }
             currentCell.append(currentPiece);
-            leaveDiv(currentCell);
+          } else {
+            startCell.append(currentPiece);
           }
+          leaveDiv(currentCell);
           currentPiece.style.position = currentPiece.style.zIndex = currentPiece.style.left = currentPiece.style.top = null;
           setBoardSize();
-        };
+        }
+        document.addEventListener('mouseup', mouseUp);
       };
 
       const cell = document.getElementById(String(i) + String(j));
